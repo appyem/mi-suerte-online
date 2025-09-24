d91b7b2d67232e46d3dd4e5c4a8d211f2ebc55fe
appyem <appyempresa@gmail.com>
2025-09-24 21:57:35 +0200

    Configurado Tailwind CSS para producción

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+﻿/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+    "./*.{js,jsx}"  // ← Esto es crucial para tu estructura sin src/
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
