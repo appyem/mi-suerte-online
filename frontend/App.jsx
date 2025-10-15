eca408c800a451f0a3172f644a859f64ece0464f
appyem <appyempresa@gmail.com>
2025-10-14 22:49:39 -0500

    detalles 2

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -2066,7 +2066,6 @@ COMISIÓN TOTAL: $${currentReport.totalCommission}
               </div>
             </div>
           )}
-		  
 		  {showTicketDetailsModal && selectedTicketDetails && (
             <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-4">
               <div className="bg-white rounded-xl max-w-2xl w-full max-h-[90vh] overflow-y-auto">
