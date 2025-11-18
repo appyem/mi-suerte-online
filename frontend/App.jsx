ea0daff77021fb781870ff49b5ead107fe5fad1a
appyem <gerenteappyempresa@gmail.com>
2025-11-18 14:48:52 -0500

    prueba colores

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1602,7 +1602,7 @@ COMISIÓN TOTAL: $${currentReport.totalCommission}
                           number: currentBet.number.slice(0, parseInt(newDigits))
                         });
                       }}
-                      className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                      className="w-full px-4 py-3 border border-gray-800 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent"
                     >
                       <option value="2">2 Cifras</option>
                       <option value="3">3 Cifras</option>
